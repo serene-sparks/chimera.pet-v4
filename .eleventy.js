62ac1ca267d56d215b26bbee75c61fb05cedb565
chimeridae <chimeranull@gmail.com>
2025-03-07 00:40:10 -0600

    initial commit

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,18 @@
-
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/js");
-// Defines shortcode for generating post excerpts
+  // Defines shortcode for generating post excerpts
   eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
 };
 
-  return {
-    passthroughFileCopy: true,
-    dir: {
-      input: "src",
-      output: "public",
-      includes: "_includes",
-    },
+
+module.exports.config = {
+  passthroughFileCopy: true,
+  dir: {
+    input: "src",
+    output: "public",
+    includes: "_includes",
+  },
 };
-
-
