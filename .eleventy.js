3618822e8b4d98c34e90bf4a1cd97a989f2eb306
chimeridae <chimeranull@gmail.com>
2025-03-24 20:53:36 -0500

    i please help i'm scared

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,46 @@
+import { DateTime } from "luxon";
 
+// See https://moment.github.io/luxon/#/zones?id=specifying-a-zone
+// Use the TZ identifier https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+const TIME_ZONE = "America/Chicago";
+const FRONT_MATTER_DATE_FORMAT = "MM-dd-yy hh:mm"; // 12-31-20 23:59
+const READABLE_DATE_FORMAT = "dd LLLL yyyy"; // 01 January 2020
 
-// 
-
-module.exports = function (eleventyConfig) {
+export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/img");
   eleventyConfig.addPassthroughCopy("./src/fonts");
   eleventyConfig.addPassthroughCopy("./src/js");
+
   // Defines shortcode for generating post excerpts
+
+  // Datetime parsing
+  eleventyConfig.addDateParsing(function(dateValue) {
+    if (dateValue instanceof Date) {
+      return dateValue;
+    } else if (typeof dateValue === "string") {
+      return DateTime.fromFormat(dateValue, "MM-dd-yy hh:mm", { zone: TIME_ZONE });
+    } else {
+      return dateValue;
+    }
+  });
+
+  // Human friendly date formatting
+  eleventyConfig.addFilter("readableDate", function(dateObj) {
+    if (dateObj instanceof DateTime) {
+      return dateObj.toFormat("dd LLLL yyyy");
+    } else if (dateObj instanceof Date) {
+      return DateTime.fromJSDate(dateObj).toFormat("dd LLLL yyyy");
+    } else {
+      return "Unknown date";
+    }
+  });
 };
 
 
-module.exports.config = {
+
+
+export const config = {
   passthroughFileCopy: true,
   dir: {
     input: "src",
@@ -20,4 +49,4 @@ module.exports.config = {
   },
 };
 
-//
+
--- src/js/global.js
+++ src/js/global.js
@@ -1,6 +1,57 @@
 $('.icon,.wooden').hover(function() {
-    var a = Math.random() * 15 - 5;
+    const a = Math.random() * 15 - 5;
     $(this).css('transform', 'rotate(' + a + 'deg) scale(1.07)');
 }, function() {
     $(this).css('transform', 'none');
 });
+
+// $(document).ready(function() {
+//     const currentYear = new Date().getFullYear();
+//     $('#copyright-date').text('est. 2023 - ' + currentYear);
+// });
+
+// to ethan if you read this: while i appreciate you adding this, i want the footer to say 20XX on pur
+
+tippy('a[title]', {
+    theme: 'custom',
+    followCursor: true,
+    arrow: tippy.narrowArrow,
+    zIndex: 9999999999,
+    maxWidth: 300,
+
+    content(reference) {
+      const title = reference.getAttribute('title');
+      reference.removeAttribute('title');
+      return title;
+    },
+    
+  }),
+  tippy('span[title]', {
+    theme: 'custom',
+    followCursor: true,
+    arrow: tippy.narrowArrow,
+    zIndex: 9999999999,
+    maxWidth: 300,
+
+    content(reference) {
+      const title = reference.getAttribute('title');
+      reference.removeAttribute('title');
+      return title;
+    },
+    
+  }),
+  tippy('img[title]', {
+    theme: 'custom',
+    followCursor: true,
+    arrow: tippy.narrowArrow,
+    zIndex: 9999999999,
+    maxWidth: 300,
+
+    content(reference) {
+      const title = reference.getAttribute('title');
+      reference.removeAttribute('title');
+      return title;
+    },
+    
+  });
+
