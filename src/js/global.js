656c974cbd0e5335280a4fd613815d74a5f8958b
chimeridae <chimeranull@gmail.com>
2025-03-25 22:07:13 -0500

    remove node_modules

--- src/js/global.js
+++ src/js/global.js
@@ -5,18 +5,19 @@ $('.icon,.wooden').hover(function() {
     $(this).css('transform', 'none');
 });
 
+$('.gallery-holder .inner').hover(function() {
+  const a = Math.random() * 15 - 5;
+  $(this).css('transform', 'rotate(' + a + 'deg) scale(1.2)');
+}, function() {
+  $(this).css('transform', 'none');
+});
+
 // $(document).ready(function() {
 //     const currentYear = new Date().getFullYear();
 //     $('#copyright-date').text('est. 2023 - ' + currentYear);
 // });
 
-// dynamically assign email link to avoid spam bots
-$(document).ready(function() {
-    $('#email-link').attr('href', 'mailto:mail@chimera.pet');
-});
-
 // to ethan if you read this: while i appreciate you adding this, i want the footer to say 20XX on pur
-// to chimera if you read this: Sounds good!
 
 tippy('a[title]', {
     theme: 'custom',
@@ -30,7 +31,7 @@ tippy('a[title]', {
       reference.removeAttribute('title');
       return title;
     },
-
+    
   }),
   tippy('span[title]', {
     theme: 'custom',
@@ -44,7 +45,7 @@ tippy('a[title]', {
       reference.removeAttribute('title');
       return title;
     },
-
+    
   }),
   tippy('img[title]', {
     theme: 'custom',
@@ -58,6 +59,6 @@ tippy('a[title]', {
       reference.removeAttribute('title');
       return title;
     },
-
+    
   });
 
